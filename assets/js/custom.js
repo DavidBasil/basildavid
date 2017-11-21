80a7a3e277a4323bb7a37ecf0071a75e5e077da5
David Basil <info@basildavid.com>
2017-11-21 08:34:34 +0400

    modify index.html

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,51 +1,11 @@
-$(document).ready(function() {
-		// semantic ui
-	$('body').transition({
-		animation: 'fade in',
-		duration: 1000
-	})
-	// modal
-	$('#portfolioButton').on('click', function(){
-			$('.ui.modal')
-			.modal({
-				'transition': 'horizontal flip'
-			})
-				.modal('show')
-	})
-	$('.ui.embed').embed()
+$('#contact-btn').on('click', function(){
+	$('.shape').shape('set next side', '#contact').shape('flip over')
+})
 
-		// contact form
-    $('#contact-form').on('submit', function(e) {
-        e.preventDefault();
-        
-        //get the name field value
-        var name = $('#name').val();
-        //get the name field value
-        var email = $('#email').val();
-        //get the comments
-        var message = $('#message').val();
-                    
-        //pretend we don't need validation
-        
-        //send to formspree
-        $.ajax({
-            url:'https://formspree.io/info@basildavid.com',
-            method:'POST',
-            data:{
-                name:name,
-                _replyto:email,
-                message:message,
-                _subject:'My Form Submission',
-            },
-            dataType:"json",
-            success:function() {
-                console.log('success'); 
-                $('#contact-form').hide(1000);
-                $('#thankyouBlock').show(1000);
-            }   
+$('#portfolio-btn').on('click', function(){
+	$('.shape').shape('set next side', '#portfolio').shape('flip over')
+})
 
-        });     
-        
-    });
-
-}); 
+$('#about-btn').on('click', function(){
+	$('.shape').shape('set next side', '#about').shape('flip down')
+})
